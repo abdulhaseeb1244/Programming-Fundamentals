3af06dd429c55ad73640ac238cebe1eccdb18873
abdulhaseeb1244 <ah046810@gmail.com>
2024-10-31 12:22:43 +0500

    done

---
+++ Runtime Error.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 10;
+    int b = 0;
+    int result = a / b;  // Division by zero error
+    cout << "Result: " << result << endl;
+
+    return 0;
+}
+
---
+++ code.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Escape Sequences in C++\n";
+
+    cout << "1. Newline:\n";
+    cout << "   This text is on a new line after the newline escape sequence.\n\n";
+
+    cout << "2. Tab:\n";
+    cout << "   This text\tis separated by a tab.\n\n";
+
+    cout << "3. Backslash:\n";
+    cout << "   To display a backslash, use \\\\ like this.\n\n";
+
+    cout << "4. Single Quote:\n";
+    cout << "   It\'s easy to use single quotes within text.\n\n";
+
+    cout << "5. Double Quote:\n";
+    cout << "   He said, \"Hello, World!\"\n\n";
+
+    cout << "6. Carriage Return:\n";
+    cout << "   This line will be overwritten by carriage return.\rOverwritten Text\n\n";
+
+    cout << "7. Alert:\n";
+    cout << "   An alert sound will play now.\a\n\n";
+
+    cout << "8. Vertical Tab:\n";
+    cout << "   Line with vertical tab\vThis appears below after a gap.\n\n";
+
+    return 0;
+}
+
